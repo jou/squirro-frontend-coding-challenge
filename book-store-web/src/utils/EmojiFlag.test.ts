0241a7958805616f56a2b482070577541087a238
Jiayong Ou <code@jou.orly.ch>
2022-05-15 22:36:50 +0200

    Add tests for EmojiFlag.ts

--- book-store-web/src/utils/EmojiFlag.test.ts
+++ book-store-web/src/utils/EmojiFlag.test.ts
@@ -3,10 +3,30 @@ import { flagFromAlpha2Code } from "~/utils/EmojiFlag";
 
 describe("EmojiFlag", () => {
     describe("flagFromAlpha2Code()", () => {
-        it("should throw an error with invalid country code", () => {
+        it("should throw on country code with invalid character", () => {
             expect(() => {
-                flagFromAlpha2Code("12");
+                flagFromAlpha2Code("1A");
             }).toThrow();
+        });
+
+        it("should throw on country code that is too short", () => {
+            expect(() => {
+                flagFromAlpha2Code("a");
+            }).toThrow();
+        });
+
+        it("should throw on country code that is too long", () => {
+            expect(() => {
+                flagFromAlpha2Code("aaa");
+            }).toThrow();
+        });
+
+        it("should return the country's flag as an emoji", () => {
+            expect(flagFromAlpha2Code("ch")).toEqual("🇨🇭");
+        });
+
+        it("should be case insensitive", () => {
+            expect(flagFromAlpha2Code("CH")).toEqual(flagFromAlpha2Code("ch"));
         });
     });
 });
